fb241abf52f3dae09a3082d27df8f0fadd4a3d66
davindb <davindbhagaspati@gmail.com>
2022-01-26 08:53:52 +0700

    Insyaallah final lagi

--- public/src/script.js
+++ public/src/script.js
@@ -1,4 +1,6 @@
-const url = `https://sistem-monitoring-suhu-iot.herokuapp.com/temperatures`;
+// const url = `https://sistem-monitoring-suhu-iot.herokuapp.com/temperatures`;
+const url = `http://localhost:5000/temperatures`;
+
 const konstantaMinyak = 1.7749;
 let tempData,
   currTemp,
@@ -11,6 +13,8 @@ let tempData,
   currLen;
 
 const delayTimeArr = [];
+lastLen = 0;
+let stop = false;
 
 setInterval(function () {
   $.ajax({
@@ -31,13 +35,45 @@ setInterval(function () {
     });
 
   currTemp = tempData[tempData.length - 1];
-  if (!currTemp) return;
+  if (!currTemp) {
+    return;
+  }
+
+  currLen = tempData.length;
+
+  if (lastLen === currLen) {
+    tempData = [];
+    indikator(0);
+    $("#status_stat").text("SYSTEM IS OFF");
+    $("#status_card").css({ background: "rgb(215,215,215)" });
+    $("#temp_stat_atas").text("OFF");
+    $("#temp_stat_samping").text("OFF");
+    $("#temp_stat_minyak").text("NONE");
+    $(".led_color").css({ background: "rgb(255,255,255)" });
+    return;
+  }
+
+  lastLen = tempData.length;
 
   // begin:: Calculate the delay time
   delayTime = new Date() - new Date(currTemp.date);
-  if (delayTime > 500) {
-    delayTime -= 500;
+  if (String(delayTime).length >= 2) {
+    delayTime = String(delayTime).slice(
+      String(delayTime).length - 2,
+      String(delayTime).length
+    );
+    delayTime = Number(delayTime);
   }
+
+  // begin:: Clear resource
+  if (delayTimeArr.length >= 40) {
+    delayTimeArr = delayTimeArr.slice(
+      delayTimeArr.length - 19,
+      delayTimeArr.length
+    );
+  }
+  // end:: Clear resource
+
   // console.log(`Current Delay: ${delayTime}`);
   delayTimeArr.push(delayTime);
   const sum = delayTimeArr.reduce((a, b) => a + b, 0);
@@ -67,19 +103,8 @@ setInterval(function () {
     currTemp.temp_status != "undefined" ? `${tempMinyak} °C` : tempMinyak
   );
 
-  if (currTemp) {
-    statusCheck(currTemp.temp_status);
-  }
+  statusCheck(currTemp.temp_status);
 
-  lastLen = tempData.length;
-  if (lastLen === currLen && currLen !== 0) {
-    tempData = [];
-    indikator(0);
-    $("#temp_stat_atas").text("NOT DETECTED");
-    $("#temp_stat_samping").text("NOT DETECTED");
-    $("#temp_stat_minyak").text("NONE");
-    $(".led_color").css({ background: "rgb(255,255,255)" });
-  }
   // Updating chart
   const maxDataLength = 20;
   let init;
